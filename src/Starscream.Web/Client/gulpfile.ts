0c4651a7ec8257c1057d1dedbb9fd049ebc1698e
Dante Castillo Dubón <dante.dubon@gmail.com>
2015-02-27 11:31:17 -0600

    compiling dev without maps

--- src/Starscream.Web/Client/gulpfile.ts
+++ src/Starscream.Web/Client/gulpfile.ts
@@ -3,17 +3,27 @@
 var gulp = require('gulp');
 var tsc  = require('gulp-tsc');
 var config = require('./gulp.config')();
+var args = require('yargs').argv;
 
 gulp.task('build', ['compile:typescript']);
-gulp.task('compile:typescript', () => {
+
+gulp.task('compile:typescript', function() {
+    
+console.log('Compiling Typescript files');
+  var maps: boolean = false;
+   if (args.dev) {
+       maps = true;
+   }
+    console.log('Ambiente Dev ' + maps);
     return gulp
         .src(config.allts)
         .pipe( tsc({
             module: 'CommonJS',
-            sourcemap: true,
+            sourcemap: maps,
             emitError: false
         })) .pipe(
         gulp.dest(config.build)
         );
-});
+}
+);
 
--- src/Starscream.Web/Client/tmp/tsreq/gulpfile.js
+++ src/Starscream.Web/Client/tmp/tsreq/gulpfile.js
@@ -2,11 +2,17 @@
 var gulp = require('gulp');
 var tsc = require('gulp-tsc');
 var config = require('./gulp.config')();
+var args = require('yargs').argv;
 gulp.task('build', ['compile:typescript']);
 gulp.task('compile:typescript', function () {
+    var maps = false;
+    if (args.dev) {
+        maps = true;
+    }
+    console.log('Ambiente Dev ' + maps);
     return gulp.src(config.allts).pipe(tsc({
         module: 'CommonJS',
-        sourcemap: true,
+        sourcemap: maps,
         emitError: false
     })).pipe(gulp.dest(config.build));
 });
