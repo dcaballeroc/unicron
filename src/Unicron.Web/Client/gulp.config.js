0ed82e7ff8e9c56bff3c2fbd712fe65e4f60104f
Dante Castillo Dubón <dante.dubon@gmail.com>
2015-04-06 19:11:07 -0600

    ordered gulp config

--- src/Unicron.Web/Client/gulp.config.js
+++ src/Unicron.Web/Client/gulp.config.js
@@ -16,166 +16,166 @@ module.exports = function() {
     var specRunnerFile = '/specs/specs.html';
     var specRunner = src + specRunnerFile;
     var config = {
-        /**
-         * Files paths
-         */
-        sourceTs:[
-            src + ts,
-            '!' + src + specs
-        ],
-        sourceHtmls: [
-                        src + htmls,
-                        '!' + index,
-                        '!' + specRunner
-                     ],
-        sourceSpecs: [
-            src + 'specs/' + specs
-        ],
-        build: './build/',
-        buildTs: build + ts,
-        buildJs: buildJs,
-        buildHtmls: build + htmls,
-        buildSpecs: buildSpecs,
-        src: src,
-        root: root,
-        fonts: './bower_components/font-awesome/fonts/**/*.*',
-        images: src + 'images/**/*.*',
-        compiledJs: [
-            build +  '**/*.module.js',
-            build + js
-        ],
-        css: temp + 'styles.css',
-        buildMaps: build + js + '.map',
-        configTs:[
-           './*.ts'
-        ],
-        temp: temp,
-        less: src + 'styles/styles.less',
-        index: index,
-        buildIndex: build + 'index.html',
-        /**
-        + TSC options
-        */
-        tsc: {
-             module: 'commonjs',
-             declarationFiles: true,
-             emitError: false
-        },
-        /**
-        * SourceMaps Options
-        **/
-        sourceMaps: {
-           pathToWrite: '../build',
-           configMaps:  {
-            includeContent: false,
-            sourceRoot: '/build/app'
-            }
-        },
-         sourceMapsForSpecs: {
-           pathToWrite: '/',
-           configMaps:  {
-            includeContent: false,
-            sourceRoot: '/src/'
-            }
-        },
-        /**
-         * Bower and NPM locations
-         */
-        bower: {
-            json: require('./bower.json'),
-            directory: './bower_components/',
-            ignorePath: '../..'
-        },
-        packages : [
-            './package.json',
-            './bower.json'
-        ],
-         /**
-         * optimized files
-         */
-        optimized: {
-            app: 'app.js',
-            lib: 'lib.js'
-        },
-         /**
-         * template cache
-         */
-        templateCache: {
-            file: 'templates.js',
-            options: {
-                module: 'app.core',
-                standAlone: false
-                
-            }
-        },
-          /**
-         * Karma and testing settings
-         */
-        specHelpers: [src + 'test-helpers/*.*'],
-        serverIntegrationSpecs: [src + 'specsIntegration/**/*.spec.*'],
-        compiledSpecs: buildSpecs + '**/*.js',
-        /**
-         * specs.html, our HTML spec runner
-         */
-        specRunner: specRunner,
-        specRunnerFile: specRunnerFile,
-        /*
-         * bootlint settings
-         */
-        bootlint: {
-            disableRules: ['W001', 'W005']
-        },
-        testlibraries: [
-           'node_modules/mocha/mocha.js',
-           'node_modules/chai/chai.js',
-           'node_modules/mocha-clean/index.js',
-           'node_modules/sinon-chai/lib/sinon-chai.js'
-        ],
-       /**
-         * Nancy Settings
-         */
-        defaultPort: 5976,
-        subdomain: 'unicron'
-    };
+                /**
+                 * Files paths
+                 */
+                sourceTs:[
+                    src + ts,
+                    '!' + src + specs
+                ],
+                sourceHtmls: [
+                                src + htmls,
+                                '!' + index,
+                                '!' + specRunner
+                             ],
+                sourceSpecs: [
+                    src + 'specs/' + specs
+                ],
+                build: './build/',
+                buildTs: build + ts,
+                buildJs: buildJs,
+                buildHtmls: build + htmls,
+                buildSpecs: buildSpecs,
+                src: src,
+                root: root,
+                fonts: './bower_components/font-awesome/fonts/**/*.*',
+                images: src + 'images/**/*.*',
+                compiledJs: [
+                    build +  '**/*.module.js',
+                    build + js
+                ],
+                css: temp + 'styles.css',
+                buildMaps: build + js + '.map',
+                configTs:[
+                   './*.ts'
+                ],
+                temp: temp,
+                less: src + 'styles/styles.less',
+                index: index,
+                buildIndex: build + 'index.html',
+                /**
+                + TSC options
+                */
+                tsc: {
+                    module: 'commonjs',
+                    declarationFiles: true,
+                    emitError: false
+                },
+                /**
+                * SourceMaps Options
+                **/
+                sourceMaps: {
+                    pathToWrite: '../build',
+                    configMaps:  {
+                        includeContent: false,
+                        sourceRoot: '/build/app'
+                    }
+                },
+                sourceMapsForSpecs: {
+                    pathToWrite: '/',
+                    configMaps:  {
+                        includeContent: false,
+                        sourceRoot: '/src/'
+                    }
+                },
+                /**
+                 * Bower and NPM locations
+                 */
+                bower: {
+                    json: require('./bower.json'),
+                    directory: './bower_components/',
+                    ignorePath: '../..'
+                },
+                packages : [
+                    './package.json',
+                    './bower.json'
+                ],
+                /**
+                * optimized files
+                */
+                optimized: {
+                    app: 'app.js',
+                    lib: 'lib.js'
+                },
+                /**
+                * template cache
+                */
+                templateCache: {
+                    file: 'templates.js',
+                    options: {
+                        module: 'app.core',
+                        standAlone: false
+
+                    }
+                },
+                /**
+                * Karma and testing settings
+                */
+                specHelpers: [src + 'test-helpers/*.*'],
+                serverIntegrationSpecs: [src + 'specsIntegration/**/*.spec.*'],
+                compiledSpecs: buildSpecs + '**/*.js',
+                /**
+                 * specs.html, our HTML spec runner
+                 */
+                specRunner: specRunner,
+                specRunnerFile: specRunnerFile,
+                /*
+                 * bootlint settings
+                 */
+                bootlint: {
+                    disableRules: ['W001', 'W005']
+                },
+                testlibraries: [
+                   'node_modules/mocha/mocha.js',
+                   'node_modules/chai/chai.js',
+                   'node_modules/mocha-clean/index.js',
+                   'node_modules/sinon-chai/lib/sinon-chai.js'
+                ],
+                /**
+                * Nancy Settings
+                */
+                defaultPort: 5976,
+                subdomain: 'unicron'
+            };
     config.getWiredepDefaultOptions = function() {
-        var options = {
-                bowerJson: config.bower.json,
-                directory: config.bower.directory,
-                ignorePath: config.bower.ignorePath
-        };
-        return options;
-    };
+                var options = {
+                    bowerJson: config.bower.json,
+                    directory: config.bower.directory,
+                    ignorePath: config.bower.ignorePath
+                };
+                return options;
+            };
     config.karma = getKarmaOptions();
-    
+
     return config;
-    
+
     function getKarmaOptions() {
-        var options = {
-            files: [].concat(
-                bowerFiles,
-                config.specHelpers,
-                build + '**/*.module.js',
-                build + '**/*.js',
-                buildSpecs + '**/*.js',
-                buildSpecs + '**/**/*.js',
-                temp + config.templateCache.file,
-                config.serverIntegrationSpecs
-              
-            ),
-            exclude: [],
-            coverage: {
-                dir: report + 'coverage',
-                reporters: [
-                    {type: 'html', subdir: 'report-html'},
-                    {type: 'lcov', subdir: 'report-lcov'},
-                    {type: 'text-summary'}
-                ]
-            },
-            preprocessors: {}
-        };
-        options.preprocessors[buildSpecs + '**/!(*.spec)+(.js)'] = ['coverage'];
-        options.preprocessors[buildSpecs + '**/**/!(*.spec)+(.js)'] = ['coverage'];
-        options.preprocessors[buildJs] = ['coverage'];
-        return options;
-    }
+                var options = {
+                    files: [].concat(
+                        bowerFiles,
+                        config.specHelpers,
+                        build + '**/*.module.js',
+                        build + '**/*.js',
+                        buildSpecs + '**/*.js',
+                        buildSpecs + '**/**/*.js',
+                        temp + config.templateCache.file,
+                        config.serverIntegrationSpecs
+
+                    ),
+                    exclude: [],
+                    coverage: {
+                        dir: report + 'coverage',
+                        reporters: [
+                            {type: 'html', subdir: 'report-html'},
+                            {type: 'lcov', subdir: 'report-lcov'},
+                            {type: 'text-summary'}
+                        ]
+                    },
+                    preprocessors: {}
+                };
+                options.preprocessors[buildSpecs + '**/!(*.spec)+(.js)'] = ['coverage'];
+                options.preprocessors[buildSpecs + '**/**/!(*.spec)+(.js)'] = ['coverage'];
+                options.preprocessors[buildJs] = ['coverage'];
+                return options;
+            }
 };
