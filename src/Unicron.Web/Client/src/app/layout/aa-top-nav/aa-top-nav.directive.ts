ff825774a968130cbdd83549e5e395b5ded90d94
Dante Castillo Dubón <dante.dubon@gmail.com>
2015-04-09 18:47:35 -0600

    changed load gif

--- src/Unicron.Web/Client/src/app/layout/aa-top-nav/aa-top-nav.directive.ts
+++ src/Unicron.Web/Client/src/app/layout/aa-top-nav/aa-top-nav.directive.ts
@@ -1,18 +1,6 @@
 /// <reference path="../../../../typings/angularjs/angular.d.ts" />
 /// <reference path="../layout.module.ts" />
-interface ITopNavScope extends ng.IScope {
-    vm: AATopNavController;
-}
-class AATopNavController {
-    constructor($scope: ITopNavScope) {
-        $scope.vm = this;
-        console.log('hola');
-    }
-}
-appLayout.controller('aa-TopNavController',
-    ['$scope', ($scope: ITopNavScope) =>
-    new AATopNavController($scope)
-]);
+
 'use strict';
 class AATopNav implements ng.IDirective {
       bindToController: boolean = true;
