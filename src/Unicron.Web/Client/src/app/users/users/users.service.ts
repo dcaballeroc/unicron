38742fed77ee5b436a4aa25e96816bbb6cff6409
Dante Castillo Dubón <dante.dubon@gmail.com>
2015-05-08 21:17:10 -0600

    user service finished

--- src/Unicron.Web/Client/src/app/users/users/users.service.ts
+++ src/Unicron.Web/Client/src/app/users/users/users.service.ts
@@ -4,32 +4,36 @@
 interface IUserPagedRequest {
     pageNumber: number;
     pageSize: number;
-    field: number;
+    field: string;
 }
 
 interface IUserResponse {
-  id: string;
-  name: string;
-  email: string;
+    id: string;
+    name: string;
+    email: string;
 }
 
 interface IUsersService {
-    getPagedUsers(payload: IUserPagedRequest): IUserLoginResponse;
-    getUser(id: string): IUserLoginResponse;
+    getPagedUsers(payload: IUserPagedRequest): angular.IPromise<IUserResponse[]>;
+    getUser(id: string): angular.IPromise<IUserResponse>;
 }
 
 class UsersService implements IUsersService {
 
-  static $inject: any = ['httpq'];
-  /*@ngInject*/
-  constructor(private httpq: IHttpQ) {
-  }
-  getPagedUsers(payload: IUserPagedRequest): IUserLoginResponse {
-    return undefined;
-  }
-  getUser(id: string): IUserLoginResponse {
-    return undefined;
-  }
+    static $inject: any = ['httpq'];
+    /*@ngInject*/
+    constructor(private httpq: IHttpQ) {
+    }
+    getPagedUsers(payload: IUserPagedRequest): angular.IPromise<IUserResponse[]> {
+        return this.httpq.Get<IUserResponse[]>('/users?' + 'PageNumber='
+            + payload.pageNumber + '&PageSize='
+            + payload.pageSize + '&Field='
+            + payload.field);
+    }
+    getUser(id: string): angular.IPromise<IUserResponse> {
+
+        return this.httpq.Get<IUserResponse>('/user/' + id);
+    }
 }
 
 appUsers.factory('usersService', ['httpq', (httpq: IHttpQ) => new UsersService(httpq)]);
