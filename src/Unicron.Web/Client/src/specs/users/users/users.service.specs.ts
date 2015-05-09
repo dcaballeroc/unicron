8917b96dfef640ad26d60678c99b78d865e75e43
Dante Castillo Dubón <dante.dubon@gmail.com>
2015-05-08 19:12:28 -0600

    added empty specs for user service

---
+++ src/Unicron.Web/Client/src/specs/users/users/users.service.specs.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../../../typings/mocha/mocha.d.ts"/>
+/// <reference path="../../../../typings/angularjs/angular.d.ts"/>
+/// <reference path="../../../../typings/angularjs/angular-mocks.d.ts"/>
+/// <reference path="../../../../typings/sinon/sinon.d.ts"/>
+/// <reference path="../../../../typings/chai/chai.d.ts"/>
+/// <reference path="../../../../typings/sinon-chai/sinon-chai.d.ts"/>
+/// <reference path="../../../app/common/httpq/httpQ.service.ts"/>
+/* tslint:disable:typedef */
+describe('users.service', () => {
+
+});
